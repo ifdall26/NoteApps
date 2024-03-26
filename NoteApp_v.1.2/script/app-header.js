23d0b1553efd4880b391d776cf7842cdf2153ece
ifdall26 <lisyukri.ifdal@gmail.com>
2024-03-26 14:52:32 +0700

    mengubah metode layouting menggunakan css grid

--- NoteApp_v.1.2/script/app-header.js
+++ NoteApp_v.1.2/script/app-header.js
@@ -3,29 +3,29 @@ class AppHeader extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
-            <style>
-                header {
-                    background-color: #171717;
-                    color: #fff;
-                    padding: 10px 20px;
-                    text-align: center;
-                    height : 70px;
-                    position : fixed;
-                    width : 100%;
-                    top : 0;
-                    box-shadow : 0px 5px 10px 5px rgba(255, 255, 255, .3);
-                    z-index: 1000;
-                }
-                
-                h1 {
-                    margin: 0;
-                    line-height : 70px;
-                }
-            </style>
-            <header>
-                <h1><slot></slot></h1>
-            </header>
-        `;
+        <style>
+          header {
+            background-color: #171717;
+            color: #fff;
+            padding: 10px 20px;
+            text-align: center;
+            position : fixed;
+            width : 100%;
+            height : 50px;
+            top : 0;
+            box-shadow : 0px 5px 10px 5px rgba(255, 255, 255, .3);
+            z-index: 1000;
+          }
+          
+          h1 {
+            margin: 0;
+            line-height : 50px;
+          }
+        </style>
+        <header>
+          <h1><slot></slot></h1>
+        </header>
+      `;
   }
 }
 
